5d8fd08759be00fd7872063af2e29e2c503419c1
Hyunul <jungwj1023@naver.com>
2023-12-07 09:11:58 +0900

    'add_spec_api'

--- spec.py
+++ spec.py
@@ -29,3 +29,18 @@ class spec_api(Resource):
         cursor.execute("SELECT * FROM spec")
         specs = cursor.fetchall()
         return specs
+    
+    def post(self):
+        tos = request.form['type_of_spec']
+        if tos == '자격증':
+            name = request.form['name'] # 자격증명
+            date = request.form['date'] # 취득일
+            publish_org = request.form['publish_org'] # 발행기관
+            pass_sep = request.form['pass_sep'] # 합격구분
+
+            cursor.execute("INSERT INTO spec (type_of_spec, name, date, publish_org, pass_sep) VALUES (%s, %s, %s, %s, %s)", (tos, name, date, publish_org, pass_sep))
+            conn.commit()
+
+        
+
+            return {'result': 'success'}
