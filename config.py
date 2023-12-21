8222ec1e1ba3c45a32dfdf1fd658b52dc91645b1
hyunul <jungwj1023@naver.com>
2023-12-21 15:21:53 +0900

    delete_config

--- config.py
+++
@@ -1,8 +0,0 @@
-DB = {
-    'host': 'capstone-db.cfdsrnexrnxy.ap-northeast-2.rds.amazonaws.com',
-    'port': 3306,
-    'user_id': 'admin',
-    'user_pw': 'woojin1023',
-    'database': 'hr',
-    'charset': 'utf8'
-}
